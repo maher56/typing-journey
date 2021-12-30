2e2fba0a2e5363709b4eb3f894640739f7b3fc7d
maher56 <mahersefo2001@gmail.com>
2021-12-30 22:52:33 +0200

    first version

---
+++ js/main.js
@@ -0,0 +1,176 @@
+let traningTexts = [
+    "You must understand the whole of life, not just one part of it. That is why you must read, that is why you must look at the skies, why you must sing, dance and write poems, and suffer, and understand, for all that is life.",
+    "Emptiness comes as a sunset comes of an evening, full of beauty, enchantment, and richness; it comes as naturally as the blossoming of a flower.",
+    "When I kill an Arab, a Jew, a Muslim, a Hindu, a communist, whatever it is, I am killing myself. I wonder if you realise this, basically.",
+    "Society is an abstraction. Abstraction is not a reality. What is reality is relationship. The relationship between human beings has created what we call society.",
+    "A mind that is full of conclusions is a dead mind, it is not a living mind. A living mind is a free mind, learning, never concluding.",
+    "Forget all you know about yourself; forget all you have ever thought about yourself; start as if you know nothing.",
+    "It must begin with you and me. All great things start on a small scale, all great movements begin with individuals; and if we wait for collective action, such action, if it takes place at all, is destructive and conducive to further misery. So revolution must begin with you and me.",
+    "Organised religions throughout the world have laid down rules, disciplines, attitudes and beliefs. But have they resolved human suffering and the deep-rooted anxieties, guilts?",
+    "The more you know yourself, the more clarity there is. Self-knowledge has no end - you don't achieve, you don't come to a conclusion. It is an endless river.",
+    "When one loses the deep intimate relationship with nature, then temples, mosques and churches become important."
+]
+let storyTexts = [
+    `There was a girl named Laila (red hat), her mother asked her to take food to her grandmother's house and warned her not to talk to anyone on the way,\
+but on the way she saw a wolf asked to play with him, but refused and told him that she is going to her grandmother's house to give her food,\
+He suggested that she gather some flowers to give her to her grandmother. The wolf preceded her and stormed into her grandmother's house.\
+I was terrified of him and hid and the wolf sat down. Then the night came to Laila and entered the house, and saw the wolf asleep in her\
+grandmother's bed claiming that it is, and that the shape and voice changed because they are sick, and when they are wolf eating, came out\
+screaming, and sought the first person saw him, and Htaba rushed to save her and save her grandmother and killed the wolf, Laila and her\
+grandmother. There are two stories in this story: The first is that the wolf ate the grandmother, but the woodcutter when he killed him\
+took the grandmother out of his stomach, and the second novel that the wolf had tied the grandmother and imprisoned and then slept in her bed.\
+The story of Laila and the Wolf, as told by the grandson of the wolf, was a good-natured wolf. He did not like predation and eating meat, so he decided to be vegetarian and ate\
+only vegetables and herbs, left meat, and lived in the forest a wicked girl living with her grandmother called Laila. Laila went out every day to the forest and wreaked havoc on her,\
+uprooting the flowers and destroying the grass that my grandfather used to eat and feed, and ruined the beautiful appearance of the forest. My grandfather tried to talk to her again\
+and again so that she would not go back to this act, The grass was trampled and uprooted from the forest every day, and after my grandfather despaired of persuading Laila not to do\
+so again, he decided to visit her grandmother in her house to talk to her and tell her what Laila was doing. When she went to the grandmother's house and opened the doors of the door,\
+she opened it. Laila's grandmother was also evil. I ran to her stick at home and attacked my grandfather without uttering a word or doing anything to her. When the old grandmother attacked\
+my grandfather, the good wolf, And the horror that he had met and defended himself pushed away from him, the grandmother fell to the ground, and hit her head with the bed, and died the grandmother\
+of Laila evil. When he saw that my grandfather the good wolf, grief and grief, and was affected and cried and hot what he does, and he thinks the child how to live without her grandmother, how much\
+will grieve, and how you will cry, and his heart is interrupted by grief for what happened. Grandfather thought to hide the body of the old grandmother, and take her clothes and disguise in the dress of\
+Jeddah to inspire Laila that her grandmother, and tries to relieve her, and compensate her Hanan grandmother lost by her death by mistake, and when Leila returned from the forest and went to the house went\
+my grandfather and lying on the bed disguised Grandma's clothes The old woman, but the evil Laila noticed that the nose of her grandmother and her ears unusually large, and her eyes as a serious wolf, discovered\
+my grandfather's denial, and opened the door and Leila came out. From then until now it is common in the jungle and among people that my good grandfather is evil, and has eaten her grandmother.\
+Mrs Pig was very tired: 'Oh dear,' she said to her three little pigs, 'I cant do this work anymore, I'm afraid you must leave home and make your own way in the world.' So the three little pigs set off.\
+The first little pig met a man carrying a bundle of straw.\
+'Excuse me,' said the first little pig politely. 'Would you please sell some of your straw so I can make a house?'\
+The man readily agreed and the first little pig went off to find a good place to build his house.\
+The other little pigs carried on along the road and, soon, they met a man carrying a bundle of sticks.\
+'Excuse me,' said the little pig politely. 'Would you please sell me some sticks so I can build a house?'\
+The man readily agreed and the little pig said goodbye to his brother.\
+The third little pig didn't think much of their ideas:\
+'I'm going to build myself a much bigger, better, stronger house,' he thought, and he carried off down the road until he met a man with a cart load of bricks.\
+'Excuse me,' said the third little pig, as politely as his mother had taught him. 'Please can you sell me some bricks so I can build a house?'\
+'Of course,' said the man. 'Where would you like me to unload them?'\
+The third little pig looked around and saw a nice patch of ground under a tree.\
+'Over there,' he pointed.\
+They all set to work and by nighttime the house of straw and the house of sticks were built but the\
+house of bricks was only just beginning to rise above the ground. The first and second little pigs laughed, they thought their brother was really silly having to work so hard when they had finished.`
+
+,`However, a few days later the brick house was completed and looked very smartwith shiny windows, a neat little chimney and a shiny knocker on the door.\
+One starlit night, soon after they had settled in, a wolf came out looking for food. By the light of the moon he espied the first little pig's house of straw and he sidled up to the door and called:\
+'Little pig, little pig, let me come in.'\
+'No, no, by the hair of my chinny chin chin!' replied the little pig.\
+'Then I'll huff and I'll puff and I'll blow your house in!' said the wolf who was a very big, bad, and a greedy sort of wolf.\
+And he huffed, and he puffed and blew the house in. But the little pig ran away as fast as his trotters could carry him and went to the second little pig's house to hide.\
+The next night the wolf was even hungrier and he saw the house of sticks. He crept up to the door and called:\
+'Little pig, little pig, let me come in.'\
+'Oh no, not by the hair on my chinny chin chin!' said the second little pig, as the first little pig hid trembling under the stairs.\
+'Then I'll huff and I'll puff and I'll blow your house in!' said the wolf.\
+And he huffed, and he puffed and he blew the house in. But the little pigs ran away as fast as their trotters could carry them and went to the third little pig's house to hide.\
+'What did I tell you?' said the third little pig. 'It's important to build houses properly.' But he welcomed them in and they all settled down for the rest of the night.\
+The following night the wolf was even hungrier and feeling bigger and badder than ever.\
+Prowling around, he came to the third little pig's house. He crept up to the door and called:\
+'Little pig, little pig, let me come in.'\
+'Oh no, not by the hair on my chinny chin chin!' said the third little pig, while the first and the second little pigs hid trembling under the stairs.\
+'Then I'll huff and I'll puff and I'll blow your house in!' said the wolf.\
+And he huffed, and he puffed and he blew but nothing happened. So he huffed and he puffed and he blew again, even harder, but still nothing happened. The brick house stood firm.\
+The wolf was very angry and getting even bigger and even badder by the minute.\
+'I'm going to eat you all,' he growled, 'just you wait and see.'\
+He prowled round the house trying to find a way in. The little pigs trembled when they saw his big eyes peering through the window. Then they heard a scrambling sound.\
+Wolf climbing down chimney\
+'Quick, quick!' said the third little pig. 'He's climbing the tree. I think he's going to come down the chimney.'\
+The three little pigs got the biggest pan they had, and filled it full of water and put it on the fire to boil. All the time they could hear the sound of the wolf climbing the tree and then walking along the roof.\
+The little pigs held their breath. The wolf was coming down the chimney. Nearer and nearer he came until, with a tremendous splash, he landed in the pan of water.\
+'Yoweeeee!' he screamed, and shot back up the chimney thinking his tail was on fire.\
+The last the three little pigs saw of the big bad wolf was him flying over the treetops clutching a very sore tail.\
+So, the three little pigs lived happily together ever after in their very smart houses of bricks.`
+]
+
+let activeTraning = window.localStorage.getItem("activeTraning");
+if(activeTraning === "true")activeTraning = 1;
+else activeTraning = 0;
+
+$(".main-content").outerHeight($(window).height() - $("nav").outerHeight(), true);
+
+$(window).resize(() => {$(".main-content").outerHeight($(window).height() - $("nav").outerHeight(), true);}); 
+
+$(".traning").click(() => {
+    window.localStorage.setItem("activeTraning" , "true");
+    window.location.reload();
+})
+$(".story").click(() => {
+    window.localStorage.setItem("activeTraning" , "false");
+    window.location.reload();
+})
+$(`${activeTraning ? ".traning" : ".story"}`).addClass("active")
+let text;
+if(activeTraning) {
+    text = traningTexts[Math.floor(Math.random() * traningTexts.length)]
+}
+else {
+    text = storyTexts[Math.floor(Math.random() * storyTexts.length)]
+}
+let content = ""
+let rightButton = new Audio("../img/alert-234.wav")
+let wrongButton = new Audio("../img/error-1110.wav")
+text = text.split(' ');
+for(let i = 0 , j = text.length ; i < j ; i++){
+    let word = "";
+    text[i].split("").forEach((e , j) => {
+        if(i === 0 && j ===0)word +=`<span class = "active">${e}</span>`;
+        else word += `<span>${e}</span>`;
+    });
+    word += `<span class = "hidden ${i == j - 1 ? "end" : ""}">s</span>`;
+    content += `<div class = "word">${word}</div>`;
+};
+$("#traning .script").html(`<div class = "textContainer">${content}</div>`);
+let letters = 'abcdefghijklmnopqrstyvwxyzABCDEFGHIJKLMNOPQRSTYVWXYZ ';
+let wpm = 0 , endText = true;
+let date = new Date() , start = 0;
+
+fetch($(document).keydown(function (e) {
+    if(!endText || $("body").attr("id") !== "traning")return;
+    let content = $("#traning .script");
+    let current = $("#traning .script span.active");
+    current.finish();
+
+    if((current.hasClass("hidden") ? " " : current.text()) === e.key){
+        current.removeClass("active");
+        
+        if(current.next().text() == "")
+        current.parent().next().children().eq(0).addClass("active");
+        else current.next().addClass("active");        
+        
+        wpm++;
+        
+        rightButton.cloneNode().play();
+        current = $("#traning .script span.active");
+        if(typeof current.parent().prev().offset() !== "undefined"){
+            if(current.parent().prev().offset().top + 5 < current.offset().top && current.prev().text() === "")
+                content.scrollTop(content.scrollTop() + 30);
+        }
+        if(current.hasClass("end")){
+            endText = false;
+            setTimeout(() => {
+                $(".popup").css("top" , "0");
+            },500);
+        }
+    }
+    else if(letters.includes(e.key)){
+        current.css("backgroundColor" , "rgb(255 0 0 / 50%)");
+        
+        wrongButton.cloneNode().play();
+        
+        current.animate({"left" : "1px"} , 100)
+        .animate({"left" : "-1px"} , 100)
+        .animate({"left" : "1px"} , 100)
+        .animate({"left" : "-1px"} , 100)
+        .animate({"left" : "0px"} , 100)
+    }
+}));
+$(document).one("keydown" ,()=> {
+    let finished = setInterval(() => {
+        if(!endText)clearInterval(finished);
+        start+=2;
+        let speed = Math.floor((wpm / 5) / (start /60));
+        $("span.current").text(Math.max(speed , $(".speed span").text()));
+        $(".speed span").text(speed);
+        let mxSpeed = window.localStorage.getItem("maxSpeedTyping");
+        window.localStorage.setItem("maxSpeedTyping" , `${Math.max(mxSpeed , speed)}`);
+        $("span.best").text(window.localStorage.getItem("maxSpeedTyping"));
+    } , 2000)
+});
+$(".again").click(() => {
+    window.location.reload();
+});
